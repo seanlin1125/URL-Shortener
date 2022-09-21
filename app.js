8f688e491f85b2cfc7f42824f809812714de5115
seanlin1125 <sean821125@gmail.com>
2022-09-21 16:36:27 +0800

    feat: set up shortURL route and add error page

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.post('/', (req, res) => {
   const { originURL } = req.body
   const shortURL = shortenURL(5)
   const baseURL = req.headers.origin
-  console.log(baseURL)
   // 找尋第一個符合的原始連結，
   URL.findOne({ originURL })
     // 如果找不到就Create一筆資料至資料庫(含此連結及新產生的隨機五位英數亂碼)
@@ -30,6 +29,15 @@ app.post('/', (req, res) => {
       res.render("index", { baseURL, shortURL: data.shortURL })
     }
     )
+    .catch((error) => console.error(error))
+})
+
+app.get('/:shortURL', (req, res) => {
+  const { shortURL } = req.params
+  const baseURL = req.headers.host
+  const errorMessage = `Error! "${baseURL}/${shortURL}" is not found. Please contact your service provider.`
+  URL.findOne({ shortURL })
+    .then((data) => data ? res.redirect(data.originURL) : res.render('error', { errorMessage }))
     .catch((error) => console.error(error))
 })
 
