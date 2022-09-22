60fcffd842f26cedd9d0a0d2e4dd0ca1e2d54362
seanlin1125 <sean821125@gmail.com>
2022-09-22 11:07:16 +0800

    fix: modify URL schema to set unique flag

--- models/URL.js
+++ models/URL.js
@@ -3,11 +3,13 @@ const Schema = mongoose.Schema
 const urlSchema = new Schema({
   shortURL: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   originURL: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   }
 })
 module.exports = mongoose.model('URL', urlSchema)
