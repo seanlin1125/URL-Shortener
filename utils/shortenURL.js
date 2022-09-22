c05f387a678407fa6c1ef00193361c8f1041220c
seanlin1125 <sean821125@gmail.com>
2022-09-22 11:04:36 +0800

    refactor: modify shortenURL function

--- utils/shortenURL.js
+++ utils/shortenURL.js
@@ -1,19 +1,14 @@
-function sample(array) {
-  const index = Math.floor(Math.random() * array.length)
-  return array[index]
-}
-
 function shortenURL(stringLength) {
   const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
   const upperCaseLetters = lowerCaseLetters.toUpperCase()
   const numbers = '1234567890'
-
-  let collection = []
-  collection = lowerCaseLetters.split('').concat(upperCaseLetters.split(''), numbers.split(''))
+  const collection = (lowerCaseLetters + upperCaseLetters + numbers).split('')
 
   let randomString = ''
   for (let i = 0; i < stringLength; i++) {
-    randomString += sample(collection)
+    const randomIndex = Math.floor(Math.random() * collection.length)
+    const randomChar = collection[randomIndex]
+    randomString += randomChar
   }
   return randomString
 }
